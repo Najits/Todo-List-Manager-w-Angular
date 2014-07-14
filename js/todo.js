5d1388ad839db9f6321827672dd1c69c2621713e
Najits <najitsaria@gmail.com>
2014-07-13 23:47:46 -0400

    syncing angularjs stuff

---
+++ js/todo.js
@@ -0,0 +1,11 @@
+function TodoCtrl($scope) {
+	$scope.totalTodos = 4;
+
+    $scope.todos = [
+    {text: "Learn AngularJS", done: false},
+    {text: "Build an app", done: false}];
+
+    $scope.addTodo = function () {
+    	$scope.todos.push({text:{{$scope.formTodoText}}, done: false});
+    };
+}
