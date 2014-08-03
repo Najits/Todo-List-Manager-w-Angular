691aee3d94be8bdb3859ad20170d9ab8c162489f
Najits <najitsaria@gmail.com>
2014-08-03 13:24:07 -0400

    converted to use angular.module

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,29 @@
-function TodoCtrl($scope) {
-	$scope.totalTodos = 4;
+angular.module('todoApp', [])
+	.controller('TodoCtrl', ['$scope', function($scope){
 
-    $scope.todos = [
-    {text: "Learn AngularJS", done: false},
-    {text: "Build an app", done: false}];
+	    $scope.todos = [
+	    	{text: "Learn AngularJS", done: false},
+	    	{text: "Build an app", done: false}
+	    ];
 
-    $scope.addTodo = function () {
-    	$scope.todos.push({text: $scope.formTodoText, done: false});
-    };
-}
+	    $scope.addTodo = function () {
+	    	$scope.todos.push({text:$scope.formTodoText, done:false});
+	    	formTodoText = '';
+	    };
+
+	    $scope.remaining = function () {
+	    	var count = 0;
+	    	angular.forEach($scope.todos, function(todo) {
+	    		if(!todo.done) count++;
+	    	});
+	    	return count;
+	    };
+
+	    $scope.clearCompleted = function () {
+	    	var oldTodos = $scope.todos;
+	    	$scope.todos = [];
+	    	angular.forEach(oldTodos, function(todo) {
+	    		if(!todo.done) $scope.todos.push(todo);
+	    	});
+	    };
+	}]);
